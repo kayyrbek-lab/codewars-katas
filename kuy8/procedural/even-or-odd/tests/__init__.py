e8d9ae17e634d4ccb24b6e6eb1237b2410aae132
Kayyrbek Perdebekov <kayyrbekmonster@gmail.com>
2025-08-16 23:07:08 +0500

    Add tests/ module description into __init__.py

---
+++ kuy8/procedural/even-or-odd/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `tests` package includes unit tests for the functions defined in the
+`src` package.
+
+Tests are written using Python's `pytest` framework and cover a variety
+of input cases.
+
+Current module:
+- `test_even_or_odd`: Validates the correctness of the `even_or_odd` function.
+"""
