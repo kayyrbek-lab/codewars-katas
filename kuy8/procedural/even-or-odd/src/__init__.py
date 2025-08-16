b2cd16ba2a5b76aba80906a4d5c6a127a6aa73f9
Kayyrbek Perdebekov <kayyrbekmonster@gmail.com>
2025-08-16 23:24:43 +0500

    Add src/ module description into __init__.py

---
+++ kuy8/procedural/even-or-odd/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `src` package contains implementations of programming exercise,
+primarily sourced from platforms like Codewars.
+
+Each module provides a standalone function with type annotations
+and docstrings to ensure clarity and maintainability.
+
+Current module:
+- `even_or_odd`: Determines whether a given integer is even or odd.
+"""
