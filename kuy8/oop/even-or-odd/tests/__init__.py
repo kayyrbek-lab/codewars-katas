2a0821fb594f6bebeb1e288cfc0833073f5d7cb0
Kayyrbek Perdebekov <kayyrbekmonster@gmail.com>
2025-08-18 22:04:28 +0500

    Add tests/ module description into __init__.py

---
+++ kuy8/oop/even-or-odd/tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Initialization file for the `tests` package.
+
+The `tests` package contains unit tests for classes and functions defined in
+the `src` package. Tests are written using Python's built-in `unittest`
+framework and cover a variety of input cases.
+
+Included modules:
+- `test_number_classifier`: Validates the behavior and correctness of the
+`NumberClassifier` class.
+"""
