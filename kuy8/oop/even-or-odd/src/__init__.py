d0db9e2fa03b31780d6c02bf5bac320f71855c38
Kayyrbek Perdebekov <kayyrbekmonster@gmail.com>
2025-08-18 22:20:14 +0500

    Add src/ module description into __init__.py

---
+++ kuy8/oop/even-or-odd/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Intitialization file for the `src` package.
+
+This package contains the core source code for the project, including
+main logic, data models, and utility functions.
+
+Modules:
+- `number_classifier`: Defines the `NumberClassifier` class used for
+classifying integers.
+"""
